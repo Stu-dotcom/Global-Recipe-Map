7672ad10037fc4391509f7d8e91ecca56699db31
Stuart Baker <s.baker061@gmail.com>
2024-11-06 17:27:42 +0000

    initial commit with basic entity classes

---
+++ recipe/src/main/java/org/stuart/recipe/entities/Location.java
@@ -0,0 +1,78 @@
+package org.stuart.recipe.entities;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "City or region name is required")
+    private String regionName; // e.g., city, state, or region name
+
+    @NotNull(message = "Latitude is required")
+    private Double latitude;
+
+    @NotNull(message = "Longitude is required")
+    private Double longitude;
+
+    @OneToOne(mappedBy = "location", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Recipe recipe;
+
+    // Constructors
+    public Location() {
+    }
+
+    public Location(String regionName, Double latitude, Double longitude) {
+        this.regionName = regionName;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRegionName() {
+        return regionName;
+    }
+
+    public void setRegionName(String regionName) {
+        this.regionName = regionName;
+    }
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+}
+
