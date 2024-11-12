8698e096c761a184f39910fa8c6d4cc21c662334
Stuart Baker <s.baker061@gmail.com>
2024-11-12 16:58:02 +0000

    updated corsMapping

--- Global Recipe Map/back-end/src/main/java/org/stuart/backend/config/WebConfig.java
+++ Global Recipe Map/back-end/src/main/java/org/stuart/backend/config/WebConfig.java
@@ -7,18 +7,22 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000","https://global-recipe-map-1.onrender.com/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                registry.addMapping("/**") // Allow all endpoints
+                        .allowedOrigins(
+                                "https://global-recipe-map-1.onrender.com", // Render frontend URL
+                                "http://localhost:3000" // local dev
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow specified HTTP methods
+                        .allowedHeaders("*");
             }
         };
     }
 }
+
 
